c17da71734b01a542ea37f36dd085d0647269d98
tomzarhin <tomzarhin@gmail.com>
2019-09-06 13:49:25 +0300

    First commit

--- NetLab-proj.py
+++ NetLab-proj.py
@@ -1,1 +1,8 @@
-print("Hello World!")
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
