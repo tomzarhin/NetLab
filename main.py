d77285451d2d92ebc2b90e1161dbc708a0cc925c
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:35:41 +0200

    tom

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+#import firebase_admin
+#from firebase_admin import credentials
+#from firebase_admin import firestore
 
 # Use a service account
 #cred = credentials.Certificate('./serviceAccount.json')
