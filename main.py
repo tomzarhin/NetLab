c870e89d3a17717b151497d2bb881a0c37710cdf
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:23:10 +0200

    try

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
-cred = credentials.Certificate("./serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': project_id,
+})
 
+db = firestore.client()
 app = Flask(__name__)
 
 @app.route("/")
