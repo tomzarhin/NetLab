0980830ac43d60d7b0d1ba27bac5107c41b90657
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:23:43 +0200

    try

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from firebase_admin import firestore
 # Use the application default credentials
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
-  'projectId': project_id,
+  'netlab': project_id,
 })
 
 db = firestore.client()
