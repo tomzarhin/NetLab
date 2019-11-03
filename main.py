831348b0d2c13085120f38740249ceda461340c9
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:10:29 +0200

    try

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "NetLab!"
+    firebase = firebase.FirebaseApplication('https://netlab-dfb99.firebaseio.com', None)
+    result = firebase.get('/users', None)
+    return result
 
 @app.route("/login")
 def login():
     return "tom!"
 
 if __name__ == '__main__':
-    firebase = firebase.FirebaseApplication('https://netlab-dfb99.firebaseio.com', None)
-    result = firebase.get('/users', None)
-    print result
+
     app.run(host='127.0.0.1', port=8080, debug=True)
