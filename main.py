3989fbde716d3c9d908a756296c7e94a1ba690c3
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:27:35 +0200

    try

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'netlab': project_id,
-})
+# Use a service account
+cred = credentials.Certificate('./serviceAccount.json')
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 app = Flask(__name__)
