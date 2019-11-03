16a02a843b16b1d079e6ed374d57331ee2e393b2
tomzarhin <tomzarhin@gmail.com>
2019-11-03 21:08:51 +0200

    try

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from firebase import firebase
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,4 +12,7 @@ def login():
     return "tom!"
 
 if __name__ == '__main__':
+    firebase = firebase.FirebaseApplication('https://netlab-dfb99.firebaseio.com', None)
+    result = firebase.get('/users', None)
+    print result
     app.run(host='127.0.0.1', port=8080, debug=True)
