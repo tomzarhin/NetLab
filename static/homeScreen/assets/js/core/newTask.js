16cacc396f93dbf1498b4ebb92a3ac9d2880ebe5
tomzarhin <tomzarhin@gmail.com>
2019-11-15 19:25:52 +0200

    load to jexcel

---
+++ static/homeScreen/assets/js/core/newTask.js
@@ -0,0 +1,36 @@
+$(function() {
+    $('#upload-file-btn').click(function() {
+        var form_data = new FormData($('#upload-file')[0]);
+        $.ajax({
+            type: 'POST',
+            url: '/uploadfile',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(data) {
+                console.log('Success!');
+            },
+        })
+            .done(function (data) {
+                if (data.error) {
+                    confirm(data.error);
+                }
+                var excel_matrix=[];
+                excel_matrix[0]=[];
+                var i=0,j=0;
+                for (value of data.excelDetails) {
+                    if(value==','){
+                        i++;
+                        excel_matrix[i] = [];
+                        j=0;
+                    }
+                    else{
+                        excel_matrix[i][j]=value;
+                        j++;
+                    }
+                }
+                $('#my').jexcel({ data:excel_matrix, colWidths: [ 300, 80, 100 ] });
+            });
+    });
+});
