cbe8ed9aa09e1bdce2c7b5936d95ecdbb03b2794
tomzarhin <tomzarhin@gmail.com>
2019-11-28 15:56:03 +0200

    adding classes

--- static/assets/js/core/Experiment.js
+++
@@ -1,9 +0,0 @@
-class Experiment {
-    constructor(id,name,desc,username) {
-    this.id=id;
-    this.experimentName = name;
-    this.experimentDescription = desc;
-    this.username=username;
-  }
-}
-
