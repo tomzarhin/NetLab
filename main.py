6aea771d091eec58b938734a96346d6fcd875538
tomzarhin <tomzarhin@gmail.com>
2019-12-16 11:43:03 +0200

    WE HAVE K2

--- main.py
+++ main.py
@@ -1,113 +1,147 @@
+"""
+__author__ = "Tom Zarhin", "Or Edri"
+__copyright__ = "Copyright 2019, The Netlab Project"
+__credits__ = "Anat Dahan"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Tom Zarhin"
+__email__ = "Tom Zarhin@s.braude.ac.il"
+__status__ = "Production"
+"""
+
 from flask import request,json,jsonify,render_template,Flask
 from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer,SilhouetteVisualizer
 from pgmpy.models import BayesianModel
 from pgmpy.estimators import MaximumLikelihoodEstimator
-from server.models.mongoDB import *
+from server.models.mongoDB import Mongo
+import server.models.K2 as K2
 import numpy as np
 import pandas
-import server.models.K2
-
+import random
 
 app = Flask(__name__, static_url_path='/static')
+mongo=Mongo()
+#------------------functions-------------------
+def is_number(s):
+# Getting a character and returning if it is a number
+# Author: Tom Zarhin
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+def bayesValidation(data,mapping,graph_list):
+# Performing prediction using a bayesian network model
+# Author: Tom Zarhin
+
+    #values = pandas.DataFrame(np.random.randint(low=0, high=2, size=(1000, 5)),
+    #columns = ['A', 'B', 'C', 'D', 'E'])
+    random.shuffle(data)
+    train_data = data[:800]
+    predict_data = data[800:]
+    bayes_model=createBayesGraph(graph_list,mapping,train_data)
+    bayes_model.fit(data)
+    predict_data = predict_data.copy()
+    predict_data.drop('E', axis=1, inplace=True)
+    y_pred = bayes_model.predict(predict_data)
+    return y_pred
+
+def createBayesGraph(graph_list,mapping,data):
+# Creating bayesian network graph function
+# Author: Tom Zarhin
+    bayes_model = BayesianModel()
+    bayes_model.add_nodes_from(list(mapping))
+    for value in graph_list:
+        temp_list=value.split(',')
+        bayes_model.add_edge(temp_list[0],temp_list[1])
+    data_dict = {mapping[i]: data[:,i] for i in range(0, len(mapping))}
+    data_dict_pd = pandas.DataFrame(data=data_dict)
+    bayes_model.fit(data_dict_pd)
+    return(bayes_model)
+#----------------------------------------------
 
 #---------------Probably unnaceccery--------------
 @app.route('/getExperiments', methods=['GET', 'POST'])
+#Getting the experiments of the user
+#Author: Tom Zarhin
 def getExperiments():
     userName=request.form.get('userNameDB')
-    experiment_array=[]
-    experiments=db.experiments.find({"userName": userName})
-    for exp in experiments:
-        exp.pop('_id')
-        experiment_array.append(exp)
+    experiment_array=mongo.getExperiments(userName)
     return jsonify({'experiments':experiment_array})
 
 @app.route('/uploadfile', methods=['GET', 'POST'])
+#Getting file from the client and returning an array representing the file
+#Author: Tom Zarhin
 def uploadfile():
     file = request.files.getlist("file")
     df = pandas.read_excel(file[0])
     excel_values=np.array(df.values)
     excel_cols=np.array(df.columns.values)
     return jsonify({'excelDetails':excel_values.tolist(),'excelCols':excel_cols.tolist()})
+
+@app.route('/getTasks', methods=['GET', 'POST'])
+#Get The tasks of the user
+#Author: Tom Zarhin
+def getTasks():
+    idExp=request.form.get('idExp')
+    tasks_array=mongo.getTask(idExp)
+    return jsonify({'tasks':tasks_array})
 #-------------------------------------------------
 
 @app.route("/")
-def hello():
+#Getting the experiments of the user
+#Author: Tom Zarhin
+def netLabStart():
     return render_template('login.html')
 
 @app.route('/login', methods=['GET', 'POST'])
+#Getting the user details and returning his experiments if he exist or else the function returning null
+#Author: Tom Zarhin
 def login():
-    password = request.form.get('password')
     inputEmail=request.form.get('inputEmail')
-    for user in db.users.find({"userName":inputEmail, "userPassword":password}):#, "status":"Disconnected"}):
-        experiment_array = []
-        experiments = db.experiments.find({"userName": inputEmail})
-        for exp in experiments:
-            exp.pop('_id')
-            experiment_array.append(exp)
-        return jsonify({'experiments': experiment_array})
-    return jsonify({'pstatus':"NOT OK"})
+    password = request.form.get('password')
+    experiment_array=mongo.login(inputEmail,password)
+    if(experiment_array==None):
+        return jsonify({'pstatus':"NOT OK"})
+    return jsonify({'experiments': experiment_array})
 
 @app.route('/register', methods=['GET', 'POST'])
+#Getting the user details and creating a new task
+#Author: Tom Zarhin
 def register():
-    password = request.form.get('password')
     inputEmail=request.form.get('inputEmail')
+    password = request.form.get('password')
     userFullName=request.form.get('userFullName')
-    db.users.insert({
-        u'userName': u'' + inputEmail + '',
-        u'userPassword': u'' + password + '',
-        u'userFullName': u'' + userFullName + '',
-        u'status':u'Disconnected'
-    })
-    return("Registered!")
+    ack=mongo.register(inputEmail,password,userFullName)
+    return jsonify({'nextId':ack})
 
 @app.route('/createExperiment', methods=['GET', 'POST'])
+#Getting the experiment details and creating a new one
+#Author: Tom Zarhin
 def createExperiment():
-    nextId = db.experiments.find_one(sort=[("id", server.models.mongoDB.pymongo.DESCENDING)])
-    if(nextId==None):
-        nextId=1
-    else:
-        nextId= int(nextId.pop('id'))+1
-    name = request.form.get('name')
     description=request.form.get('description')
     userName=request.form.get('userName')
-    db.experiments.insert_one({
-        u'id': nextId,
-        u'experimentName': u'' + name + '',
-        u'experimentDescription': u'' + description + '',
-        u'userName': u'' + userName + '',
-        u'tasks':[]
-    })
+    name = request.form.get('name')
+    nextId=mongo.createExperiment(description,userName,name)
     return jsonify({'nextId':nextId})
 
 @app.route('/createTask', methods=['GET', 'POST'])
+#Getting the tasks details and creating a new one
+#Author: Tom Zarhin
 def createTask():
     dataset = json.loads(request.form['dataset'])
     datasetcols = json.loads(request.form['datasetcols'])
     name=request.form.get('taskname')
     description=request.form.get('taskDescription')
     current_experiment_id=int(request.form.get('current_experiment'))
-    nextId = db.experiments.find_one({"id":current_experiment_id})
-    if(len(nextId["tasks"])==0):
-        nextId=1
-    else:
-        nextId= int(nextId["tasks"][-1].pop('task_id'))+1
-    myquery = {"id":current_experiment_id }
-    newvalues = {"$push": {"tasks": {"task_id":nextId,"taskName": name,"taskDescription":description, "datasetcols":datasetcols, "dataset":dataset}}}
-    db.experiments.update_one(myquery,newvalues)
+    nextId=mongo.createExperiment(dataset,datasetcols,name,description,current_experiment_id)
     return jsonify({'idTask':nextId})
 
-@app.route('/getTasks', methods=['GET', 'POST'])
-def getTasks():
-    idExp=request.form.get('idExp')
-    tasks_array=[]
-    tasks=db.tasks.find({"idExp": idExp})
-    for task in tasks:
-        task.pop('_id')
-        tasks_array.append(task)
-    return jsonify({'tasks':tasks_array})
-
 @app.route('/goKmeans', methods=['GET', 'POST'])
+#Running Kmeans algorithm for Clustering graph
+#Author: Tom Zarhin
 def goKmeans():
     clusteringNum = request.form['clusteringNum']
     dataset = json.loads(request.form.get('dataset'))
@@ -130,6 +164,8 @@ def goKmeans():
     return jsonify({'inputArray': list(new_list),'kmeansLabels':(kmeans.labels_.tolist()),'elbowValue':str(elbow),'silhouetteValue':('%.3f' % silhouette)})
 
 @app.route('/goK2', methods=['GET', 'POST'])
+#Running K2 algorithm for Bayesian Network Correlation
+#Author: Tom Zarhin
 def goK2():
     cpds_array=[]
     categories = json.loads(request.form['datasetcols'])
@@ -138,38 +174,31 @@ def goK2():
     data = np.array(data)
 
     # initialize "the blob" and map its variable names to indicies
-    g = server.models.K2.data_blob(data)
+    g = K2.data_blob(data)
 
-    mapping = server.models.K2.map_categories(categories)
+    mapping = K2.map_categories(categories)
     # set the maximum number of parents any node can have
     iters = 1
-    p_lim_max = 2
+    p_lim_max = 1
     # iterate from p_lim_floor to p_lim_max with random restart
-    p_lim_floor = 1
+    p_lim_floor = 0
     best_score = -10e10
     best_dag = np.zeros((1, 1))
     for i in range(iters):
         for u in range(p_lim_floor, p_lim_max):
             # generate random ordering
             order = np.arange(g.var_number)
-            (dag, k2_score) = server.models.K2.k2(g, order, u, data)
+            (dag, k2_score) = K2.k2(g, order, u, data)
             score = np.sum(k2_score)
             if (score > best_score):
-                server.models.K2.best_score = score
+                K2.best_score = score
                 best_dag = dag
 
-    graph_list=server.models.K2.graph_out(dag, mapping)
+    graph_list=K2.graph_out(dag, mapping)
 
     #Finding the Conditional Probabilities Tables
-    model = BayesianModel()
-    model.add_nodes_from(list(mapping))
-    for value in graph_list:
-        temp_list=value.split(',')
-        model.add_edge(temp_list[0],temp_list[1])
-    data_dict = {mapping[i]: data[:,i] for i in range(0, len(mapping))}
-    data_dict_pd = pandas.DataFrame(data=data_dict)
-    model.fit(data_dict_pd)
-    cpds_tables=model.get_cpds()
+    bayes_model=createBayesGraph(graph_list,mapping,data)
+    cpds_tables=bayes_model.get_cpds()
     for cpd in cpds_tables:
         cpds_list={}
         if(len(cpd.values.shape)==3):
@@ -194,13 +223,6 @@ def goK2():
 
     return jsonify({'status': 'done','dataset_k2':dag.tolist(),'categories':categories,'cpt_list':cpds_array})
 
-#------------------functions-------------------
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-#----------------------------------------------
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
--- server/models/mongoDB.py
+++ server/models/mongoDB.py
@@ -1,5 +1,70 @@
 import pymongo
+class Mongo:
+    def __init__(self):
+        client = pymongo.MongoClient("mongodb+srv://admin:admin@netlab-keluq.azure.mongodb.net/netlabdb?retryWrites=true&w=majority")
+        self.db = client.netlabdb
 
-client = pymongo.MongoClient("mongodb+srv://admin:admin@netlab-keluq.azure.mongodb.net/netlabdb?retryWrites=true&w=majority")
-db = client.netlabdb
+    def getExperiments(self,userName):
+        experiment_array=[]
+        experiments=self.db.experiments.find({"userName": userName})
+        for exp in experiments:
+            exp.pop('_id')
+            experiment_array.append(exp)
+        return experiment_array
 
+    def getTask(self,idExp):
+        tasks_array=[]
+        tasks=self.db.tasks.find({"idExp": idExp})
+        for task in tasks:
+            task.pop('_id')
+            tasks_array.append(task)
+        return tasks_array
+
+    def login(self,inputEmail,password):
+        for user in self.db.users.find({"userName":inputEmail, "userPassword":password}):#, "status":"Disconnected"}):
+            experiment_array = []
+            experiments = self.db.experiments.find({"userName": inputEmail})
+            for exp in experiments:
+                exp.pop('_id')
+                experiment_array.append(exp)
+            return(experiment_array)
+        return(None)
+
+    def createExperiment(self,description,userName,name):
+        nextId = self.db.experiments.find_one(sort=[("id", pymongo.DESCENDING)])
+        if (nextId == None):
+            nextId = 1
+        else:
+            nextId = int(nextId.pop('id')) + 1
+        self.db.experiments.insert_one({
+            u'id': nextId,
+            u'experimentName': u'' + name + '',
+            u'experimentDescription': u'' + description + '',
+            u'userName': u'' + userName + '',
+            u'tasks': []
+        })
+        return nextId
+    def createTask(self,dataset,datasetcols,name,description,current_experiment_id):
+        nextId = self.db.experiments.find_one({"id": current_experiment_id})
+        if (len(nextId["tasks"]) == 0):
+            nextId = 1
+        else:
+            nextId = int(nextId["tasks"][-1].pop('task_id')) + 1
+        myquery = {"id": current_experiment_id}
+        newvalues = {"$push": {
+            "tasks": {"task_id": nextId, "taskName": name, "taskDescription": description, "datasetcols": datasetcols,
+                      "dataset": dataset}}}
+        self.db.experiments.update_one(myquery, newvalues)
+        return(nextId)
+
+    def register(self,inputEmail,password,userFullName):
+        try:
+            self.db.users.insert({
+                u'userName': u'' + inputEmail + '',
+                u'userPassword': u'' + password + '',
+                u'userFullName': u'' + userFullName + '',
+                u'status': u'Disconnected'
+            })
+            return("OK")
+        except:
+            return("except")
