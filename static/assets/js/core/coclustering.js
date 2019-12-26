aa686cdab1d2224afd60272cef6e74fdecd248f1
tomzarhin <tomzarhin@gmail.com>
2019-12-26 20:00:46 +0200

    co clustering works

--- static/assets/js/core/coclustering.js
+++ static/assets/js/core/coclustering.js
@@ -1,14 +1,7 @@
 $(document).ready(function () {
-    var idExp = JSON.parse(window.localStorage.getItem("idExp"));
-    var idTask = JSON.parse(window.localStorage.getItem("idTask"));
-    var experiments = JSON.parse(window.localStorage.getItem("experiments"));
-    var idExpArray = experiments.findIndex(x => x.id === parseInt(idExp));
-    var idTaskArray = experiments[idExpArray].task.findIndex(x => x.task_id === parseInt(idTask))
+    var task1 = JSON.parse(window.localStorage.getItem("task1"));
+    var task2 = JSON.parse(window.localStorage.getItem("task2"));
     var coclusteringtable = JSON.parse(window.localStorage.getItem("coclusteringtable"));
-
-    var colsArr = experiments[idExpArray].task[idTaskArray].datasetcols.split(",");
-    var datasetWithPrior=JSON.parse(JSON.stringify(experiments[idExpArray].task[idTaskArray].dataset));
-    var headArray=experiments[idExpArray].task[idTaskArray].datasetcols.split(",")
 
     var ctx1=document.getElementById('canvas1').getContext('2d');
     var ctx2=document.getElementById('canvas2').getContext('2d');
@@ -19,11 +12,11 @@ $(document).ready(function () {
 
     var kmeansLabels1 = JSON.parse(window.localStorage.getItem("labels1"));
     var data1 = JSON.parse(window.localStorage.getItem("task1_dataset"));
-    var dataset_clustering_cols1 = JSON.parse(window.localStorage.getItem("dataset_clustering_cols"));
+    var dataset_clustering_cols1 = task1[0].datasetcols;
 
     var kmeansLabels2 = JSON.parse(window.localStorage.getItem("labels2"));
     var data2 = JSON.parse(window.localStorage.getItem("task2_dataset"));
-    var dataset_clustering_cols2 = JSON.parse(window.localStorage.getItem("dataset_clustering_cols"));
+    var dataset_clustering_cols2 = task2[0].datasetcols;
 
     var clustering1=new Clustering(kmeansLabels1,data1,dataset_clustering_cols1,ctx1,xCord1,yCord1);
     var clustering2=new Clustering(kmeansLabels2,data2,dataset_clustering_cols2,ctx2,xCord2,yCord2);
@@ -33,8 +26,6 @@ $(document).ready(function () {
     clustering2.plotPoints();
 
     jexcelSpreadSheet = jexcel(document.getElementById('spreadsheet1'), {
-        //colHeaders: headArray,
-        //data:data.excelDetails,
         data: coclusteringtable,
         csvHeaders: false,
         loadingSpin: true,
