2f8a28003e398bef9fb14444ef5702e13988cf19
oredri <oredri2010@gmail.com>
2019-12-30 00:28:01 +0200

    fix bayes

---
+++ static/assets/js/core/register.js
@@ -0,0 +1,26 @@
+localStorage.clear();
+    window.onload = function () {
+    document.getElementById("register").addEventListener("click", function() {
+        document.getElementById("loadingbar").style.visibility = 'visible';
+        var form_data = new FormData();
+        form_data.append('inputEmail', document.getElementById("your_name").value);
+        form_data.append('password', document.getElementById("your_pass").value);
+        form_data.append('userFullName', document.getElementById("your_username").value);
+        $.ajax({
+            type: 'POST',
+            url: '/register',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function (data) {
+                console.log('Success!');
+
+            },
+        })
+            .done(function (data) {
+                if (data.error) {
+                    confirm(data.error);
+                }
+    });
+};
