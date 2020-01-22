78002ddfdabcb242cf85fdab0cb5c24e46c80cb7
tomzarhin <tomzarhin@gmail.com>
2020-01-22 19:13:39 +0200

    masive change - main

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ from tkinter import *
 import Routers as Routers
 import multiprocessing
 
+
+def number_constraint(digit):
+    v = str(digit)
+    try:
+        v = int(v)
+    except ValueError:
+        if v != "\x08" and v != "":
+            return "break"
+    return "ok"
+
 def initialclicked():
+    if(number_constraint(txt.get())=="break"):
+        return
     first_label.configure(text="Running on http://127.0.0.1:" + txt.get() + "/")
     global routers
     routers = multiprocessing.Process(target=Routers.run_server,args=(txt.get(),))
@@ -35,6 +47,7 @@ if __name__ == '__main__':
     first_label = Label(window, text="Please choose port to run localhost")
     first_label.grid(column=0, row=1)
     txt = Entry(window, width=10)
+    txt.insert(END, '1000')
     txt.grid(column=0, row=2)
     initial_btn = Button(window, text="Initialize", command=initialclicked)
     close_btn = Button(window, text="Close Connection", command=closeClicked)
